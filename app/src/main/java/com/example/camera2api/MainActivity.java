ad0eb652936bff4ffc256cf7ede02205bef5e011
rohitkumar1999 <rohit64229@gmail.com>
2019-11-25 23:42:23 +0530

    first commit

---
+++ app/src/main/java/com/example/camera2api/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.camera2api;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
